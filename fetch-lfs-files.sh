ae02b1c5f810f0108692c4c1c5c688b75f96adcc
LouCapener <louise.capener@manchester.ac.uk>
2024-05-10 13:43:54 +0100

    Update fetch LFS script to use HTTPS for LFS operations

--- fetch-lfs-files.sh
+++ fetch-lfs-files.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 echo "Starting LFS fetch script"
-git config --global url."https://github.com/".insteadOf "ssh://git@github.com/"
+echo "Configuring Git to use HTTPS for LFS operations"
+git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "ssh://git@github.com/"
+git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
+echo "Git LFS install"
 git lfs install
+echo "Fetching LFS objects"
 git lfs fetch || { echo 'Failed to fetch LFS objects'; exit 1; }
+echo "Checking out LFS objects"
 git lfs checkout || { echo 'Failed to checkout LFS objects'; exit 1; }
 echo "LFS fetch and checkout completed"
