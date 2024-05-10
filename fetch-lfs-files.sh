4644dde9bed89cc54eb1b1db64086707bb1dcf0b
LouCapener <louise.capener@manchester.ac.uk>
2024-05-10 13:24:23 +0100

    Add fetch LFS files script and Vercel configuration

--- fetch-lfs-files.sh
+++ fetch-lfs-files.sh
@@ -2,7 +2,7 @@
 
 # Exit on error
 set -e
-
+git config --global url."https://${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}@github.com/".insteadOf "https://github.com/"
 # Fetch LFS files
 git lfs install
 git lfs fetch
