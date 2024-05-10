0716489e72c1a81cc8503e9eee398b652296f5d2
LouCapener <louise.capener@manchester.ac.uk>
2024-05-10 13:35:03 +0100

    Plzzz

--- fetch-lfs-files.sh
+++ fetch-lfs-files.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-# Exit on error
 set -e
+echo "Starting LFS fetch script"
 git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
-# Fetch LFS files
 git lfs install
-git lfs fetch
-git lfs checkout
+git lfs fetch || { echo 'Failed to fetch LFS objects'; exit 1; }
+git lfs checkout || { echo 'Failed to checkout LFS objects'; exit 1; }
+echo "LFS fetch and checkout completed"
